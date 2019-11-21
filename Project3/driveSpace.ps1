225e22fdf1d52e7d26ec231e0657fc0e1b8ac713
blairtl <blairtl@mail.uc.edu>
2019-11-21 16:15:20 -0500

    Version 2

--- Project3/driveSpace.ps1
+++ Project3/driveSpace.ps1
@@ -1,33 +1,17 @@
-$disk = Get-WmiObject Win32_LogicalDisk -ComputerName $env:COMPUTERNAME -Filter "DeviceID='C:'" | Select-Object Size, FreeSpace
-"PRIMARY DRIVE"
-"(C:\) drive total size in GB: "
-[Math]::Round($disk.Size / 1GB)
-"(C:\) drive free space in GB: "
-[Math]::Round($disk.FreeSpace / 1GB)
-""
-""
+$Drives = Get-PSDrive -PSProvider 'FileSystem'
 
-$disk = Get-WmiObject Win32_LogicalDisk -ComputerName $env:COMPUTERNAME -Filter "DeviceID='D:'" | Select-Object Size, FreeSpace
-"SECONDARY DRIVE"
-"(D:\) drive total size in GB: "
-[Math]::Round($disk.Size / 1GB)
-"(D:\) drive free space in GB: "
-[Math]::Round($disk.FreeSpace / 1GB)
-""
-""
+foreach($Drive in $drives) {
 
-$disk = Get-WmiObject Win32_LogicalDisk -ComputerName $env:COMPUTERNAME -Filter "DeviceID='E:'" | Select-Object Size, FreeSpace
-"REMOVABLE DRIVE/FLASH DRIVE"
-"(E:\) drive total size in GB: "
-[Math]::Round($disk.Size / 1GB)
-"(E:\) drive free space in GB: "
-[Math]::Round($disk.FreeSpace / 1GB)
-""
-""
+    Get-ChildItem -Path $Drive.Root -Recurse -File -ErrorAction SilentlyContinue -Force |
+        Sort-Object Length -Descending |
+        Select-Object -First 5
 
-"Ten largest files found:"
-Get-ChildItem c:\ -r -ErrorAction SilentlyContinue | Where-Object {$_.permission -match "read"}| Sort-Object -descending -property length | Select-Object -first 10 name, Length | Out-GridView
+   Get-WmiObject Win32_LogicalDisk -ComputerName $env:COMPUTERNAME -Filter "DeviceID='$Drive'" | Select-Object Size, FreeSpace
 
-""
-""
-Pause
+    "($Drive\) drive total size in GB: "
+    [Math]::Round($Drive.Size / 1GB)
+    "($Drive\) drive free space in GB: "
+    [Math]::Round($Drive.FreeSpace / 1GB)
+    ""
+    ""
+}
